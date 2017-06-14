e27fff7c56789d64dab7bfd3997160bed4fe9ba3
tangdi <tangdi0802@foxmail.com>
2017-06-14 11:55:17 +0800

    原型模式完毕

--- src/prototypemodel/DeepCopyDocument.java
+++ src/prototypemodel/DeepCopyDocument.java
@@ -3,26 +3,23 @@ package prototypemodel;
 import java.util.ArrayList;
 
 /**
- * Created by Administrator on 2017/6/13.
+ * Created by Administrator on 2017/6/14.
  */
-
-/**
- * 拷贝函数，只能深拷贝基本类型，虽然String是引用类型，但是变现出值类型的特性，所以该例子中，副本中改变mText字段对原型没用影响
- */
-public class ShallowCopyWordDocument implements Cloneable {
+public class DeepCopyDocument implements Cloneable{
     private String mText;
     private ArrayList<String> mImage = new ArrayList<>();
 
-    public ShallowCopyWordDocument(){
+    public DeepCopyDocument(){
         System.out.println("--------------ShallowCopyWordDocument-------------------");
     }
 
     @Override
-    protected ShallowCopyWordDocument clone(){
+    protected DeepCopyDocument clone(){
         try {
-            ShallowCopyWordDocument document = (ShallowCopyWordDocument)super.clone();
+            DeepCopyDocument document = (DeepCopyDocument)super.clone();
             document.mText = this.mText;
-            document.mImage = this.mImage;
+            //引用类型需要调用自身的clone方法
+            document.mImage = (ArrayList<String>) this.mImage.clone();
             return document;
         } catch (CloneNotSupportedException e) {
             e.printStackTrace();
@@ -53,6 +50,6 @@ public class ShallowCopyWordDocument implements Cloneable {
         for(String imgName : mImage){
             System.out.print("image name : " + imgName);
         }
-        System.out.println("-------------------word content end-------------------");
+        System.out.print("-------------------word content end-------------------");
     }
 }
--- src/prototypemodel/PrototypemodelMain.java
+++ src/prototypemodel/PrototypemodelMain.java
@@ -19,6 +19,22 @@ public class PrototypemodelMain {
         copyWordDocument.showDocument();
 
         shallowCopyWordDocument.showDocument();
+
+        /*-------------------------------------------------deepclone-----------------------------------------*/
+        DeepCopyDocument deepCopyDocument = new DeepCopyDocument();
+        deepCopyDocument.setmText("this is a deepDocument");
+        deepCopyDocument.addImage("image_1.jpg");
+        deepCopyDocument.addImage("image_2.jpg");
+        deepCopyDocument.addImage("image_3.jpg");
+        deepCopyDocument.showDocument();
+
+        DeepCopyDocument deepCopyDocument1 = deepCopyDocument.clone();
+        deepCopyDocument1.showDocument();
+        deepCopyDocument1.setmText("this is a deep clone Document");
+        deepCopyDocument1.addImage("image_4.jpg");
+        deepCopyDocument1.showDocument();
+
+        deepCopyDocument.showDocument();
     }
 
 }
